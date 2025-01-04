ed45e51135b3b88d6c0e5ef4fe935c8dfb1a7d3c
acquijanoa <acquijanoa@unal.edu.co>
2025-01-04 13:59:13 -0500

    project LHS0005 added with a new structure

--- LHS0004/README.md
+++ LHS0004/README.md
@@ -6,3 +6,12 @@
 	Process the raw data
 
 
+### Creating the DAG
+
+Once the Snakemake workflow has been completed, a DAG graph can be created as follows, 
+
+```
+snakemake --dag | dot -Tpng > dag.png
+```
+
+It will save a file dat.png in the main folder.
--- create_project_folders.sh
+++ create_project_folders.sh
@@ -16,11 +16,28 @@ mkdir -p "$project_root/results/figures"
 mkdir -p "$project_root/results/reports"
 mkdir -p "$project_root/logs"
 mkdir -p "$project_root/config"
-mkdir -p "$project_root/archive"
+#mkdir -p "$project_root/archive"
+
+# Create config.yaml
+config_file="$project_root/config/config.yaml"
+cat <<EOL > "$config_file"
+# config.yaml
+example_key: example_value
+EOL
+echo "config.yaml file created at $config_file"
+
+# Create empty Snakefile
+snakefile="$project_root/Snakefile"
+cat <<EOL > "$snakefile"
+# Snakefile
+
+configfile: "config/config.yaml"
+EOL
 
 # Create placeholder files
 echo "Overview of the project" > "$project_root/README.md"
 echo "Detailed instructions for collaborators" > "$project_root/instructions.md"
+echo "Snakefile file created at $snakefile"
 
 # Confirmation message
 echo "Project folder structure for '$project_root' created successfully!"
